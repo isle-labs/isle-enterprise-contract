bc7b0ba2f91f09cf345f84e124d610405d0df854
wiasliaw <wiasliaw@protonmail.com>
2026-05-18 15:56:03 +0800

    feat(scripts): add DeployIsleUSD

---
+++ scripts/deployments/DeployIsleUSD.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import { DeployerActor } from "scripts/actors/Deployer.s.sol";
+import { ERC20Mint }     from "scripts/deployments/contracts/ERC20Mint.sol";
+
+/// @notice Deploys the mintable testnet IsleUSD asset.
+/// @notice usage: forge script DeployIsleUSD --rpc-url <url> --broadcast
+contract DeployIsleUSD is DeployerActor {
+    function run() public {
+        ERC20 asset = _deploy();
+        writeSingleton("IsleUSD", address(asset));
+    }
+
+    function _deploy() internal asDeployer returns (ERC20) {
+        return new ERC20Mint("Isle USD", "IUSD");
+    }
+}
